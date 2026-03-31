53534219552838f223585e6e7478f54a341c9be1
Ayc0 <ayc0.benj@gmail.com>
2026-03-31 21:40:00 +0000

    Update to eslint 10 & remove ts-node

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
-import type { RuleConfig as PreferredExportDefaultNamingRuleConfig } from "./preferred-export-default-naming";
-import type { RuleConfig as PreventImportsRuleConfig } from "./prevent-imports";
+import type { RuleConfig as PreferredExportDefaultNamingRuleConfig } from "./preferred-export-default-naming.js";
+import type { RuleConfig as PreventImportsRuleConfig } from "./prevent-imports.js";
 
 export type Configs = {
   "preferred-export-default-naming": PreferredExportDefaultNamingRuleConfig;
--- tests/_setup.js
+++
@@ -1,6 +0,0 @@
-const { describe, it } = require("node:test");
-
-globalThis.describe = describe;
-globalThis.it = it;
-
-require("ts-node").register({ transpileOnly: true });
